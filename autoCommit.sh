4b632f749f1e6a9fe11ffda77d50d955f3f99a50
Lex-ssf2 <jossnedjosue@gmail.com>
2025-04-10 16:07:37 -0400

    Commit automatico en monitoreo

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,3 +1,7 @@
+git add monitoreo/
+git add monitoreo/monitoreo_sistema.csv
+git add README.md
+git add autoCommit.sh
 git commit -m "Commit automatico en monitoreo"
 git push origin monitoreo
 echo "Rama subida a GitHub completado con éxito!"
